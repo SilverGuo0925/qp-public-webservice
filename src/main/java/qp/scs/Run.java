1d00197a4c05d632024c2c1ca6865f43a67a72ce
silver.guo <silver.guo@wizvision.com>
2019-12-11 15:52:18 +0800

    second commit

--- src/main/java/qp/scs/Run.java
+++ src/main/java/qp/scs/Run.java
@@ -12,14 +12,5 @@ public class Run {
   public static void main(String[] args) {
     SpringApplication.run(Run.class, args);
   }
-  
-//    @Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-//			}
-//		};
-//	}
+
 }
