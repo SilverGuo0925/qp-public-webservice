bea028362409b7e5f1f02931aedb7809d3e2d492
silver.guo <silver.guo@wizvision.com>
2020-02-18 14:11:35 +0800

    update spring boot for tomcat

--- src/main/java/qp/scs/Run.java
+++ src/main/java/qp/scs/Run.java
@@ -2,12 +2,13 @@ package qp.scs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Run {
+public class Run  extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(Run.class, args);
